efaf57e8fb22143b1d7d49060c68d94b2602dabd
Kayode <kayode.awe@outlook.com>
2022-07-11 10:55:42 -0600

    Completed code for sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+
+
+    // Check for negative values
+    if ((num1 < 1 || num2 < 1)) return "ERROR";
+
+    // Check for if any of the parameters is not a number
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    
+    // if the first number is greater than the second number, swap them!
+    if (num1 > num2) {
+        const temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
+    let sum = 0;
+
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
