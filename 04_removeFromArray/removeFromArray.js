65d2b2ba124b8e242c6d7740c6d75f1aaa573d97
Kayode <kayode.awe@outlook.com>
2022-07-10 23:32:02 -0600

    Complete tasks on removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(...num) {
 
+    const array = num[0];
+    const newArray = [];
+
+    array.forEach(element => {
+
+        if (!num.includes(element)) {
+            newArray.push(element);
+        }
+        
+    });
+    return newArray;
 };
 
 // Do not edit below this line
